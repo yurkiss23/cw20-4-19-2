c153c12f2e47efc8837a82d5632d8bfd433357ae
User <User@A15COMP6>
2019-04-20 13:19:59 +0300

    stop step

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,56 @@ namespace WpfApp2
         public MainWindow()
         {
             InitializeComponent();
+            List<User> users = new List<User>();
+            users.Add(new User() { Id = 1, Name = "John Doe", Birthday = new DateTime(1971, 7, 23) });
+            users.Add(new User() { Id = 2, Name = "Jane Doe", Birthday = new DateTime(1974, 1, 17),
+                ImageUrl = "https://vasuluna2x.files.wordpress.com/2012/04/yak-zminiti-oval-oblichchya1.jpg" });
+            users.Add(new User() { Id = 3, Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2) });
+
+            dgSimple.ItemsSource = users;
+        }
+    }
+
+    public class User : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+
+        private string name;
+        private DateTime birthday;
+
+        public DateTime Birthday
+        {
+            get { return this.birthday; }
+            set
+            {
+                if (this.birthday != value)
+                {
+                    this.birthday = value;
+                    this.NotifyPropertyChanged("Birthday");
+                }
+            }
+        }
+        public string ImageUrl { get; set; }
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.name != value)
+                {
+                    this.name = value;
+                    this.NotifyPropertyChanged("Name");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
         }
     }
 }
