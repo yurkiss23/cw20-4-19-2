17cf2d55bd35ae0926166e0353008ca0586fd356
yurkiss <terranet23@gmail.com>
2019-04-29 21:02:15 +0300

    edit adduserwindow

--- WpfApp2/Windows/AddUserWindow.xaml.cs
+++ WpfApp2/Windows/AddUserWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -22,23 +23,61 @@ namespace WpfApp2.Windows
     public partial class AddUserWindow : Window
     {
         private FileInfo _imgUser;
+        public string NameAdd { get; set; }
+        public DateTime BirthAdd { get; set; }
+        public string ImgUrlAdd { get; set; } = @"\Images\NoImage.jpg";
         public AddUserWindow()
         {
             InitializeComponent();
+            imgUser.Source = new BitmapImage(new Uri(ImgUrlAdd, UriKind.Relative));
         }
 
-        private void LblFhoto_Click(object sender, RoutedEventArgs e)
-        {
+        //private void LblFhoto_Click(object sender, RoutedEventArgs e)
+        //{
 
-        }
+        //}
 
-        private void BtnFhoto_Click(object sender, RoutedEventArgs e)
+        //private void BtnFhoto_Click(object sender, RoutedEventArgs e)
+        //{
+
+        //}
+
+        private void BtnImg_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
-            ofd.ShowDialog();
-            txtImg.Text = ofd.FileName;
-            _imgUser = new FileInfo(txtImg.Text).CopyTo(@"C:\Users\yurkiss\source\repos\cw20-4-19-2\WpfApp2\Images");
-            //imgUser
+            if (ofd.ShowDialog() == true)
+            {
+                MessageBox.Show("if");
+                txtImg.Text = ofd.FileName;
+                _imgUser = new FileInfo(txtImg.Text);
+                _imgUser.CopyTo($@"C:\Users\Public\Pictures\{txtName.Text}.jpg");
+                ImgUrlAdd = ConfigurationManager.AppSettings["img"] + txtName.Text + ".jpg";
+            }
+            imgUser.Source = new BitmapImage(new Uri(ImgUrlAdd, UriKind.RelativeOrAbsolute));
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            NameAdd = txtName.Text;
+            BirthAdd = dpbirthday.DisplayDate;
+            this.Close();
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            NameAdd = null;
+
+            //BirthAdd = dpbirthday.DisplayDate;
+            ImgUrlAdd = null;
+            this.Close();
+        }
+
+        private void inputText(object sender, RoutedEventArgs e)
+        {
+            dpbirthday.IsEnabled = true;
+            btnImg.IsEnabled = true;
+            txtImg.IsEnabled = true;
+            btnAdd.IsEnabled = true;
         }
     }
 }
